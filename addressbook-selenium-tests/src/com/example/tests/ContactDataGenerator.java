349e24896c74b0cd2b3630a0821068e37c26d9e0
makarkina <anna.makarkina@gmail.com>
2015-03-17 22:52:06 -0400

    ДЗ11

---
+++ addressbook-selenium-tests/src/com/example/tests/ContactDataGenerator.java
@@ -0,0 +1,166 @@
+package com.example.tests;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import com.thoughtworks.xstream.XStream;
+
+public class ContactDataGenerator {
+
+	public static void main(String[] args) throws IOException {
+		if (args.length < 3){
+			System.out.println("Please, specify parameters: <amount of test data>, <file>, <format>");
+		}
+		
+		int amount = Integer.parseInt(args[0]);
+		File file = new File(args[1]);
+		String format = args[2];
+		
+		if (file.exists()){
+			System.out.println("File exists, please remove it manually: " + file);
+			return;
+		}
+		
+		List<ContactData> contacts = generateRandomContacts(amount);
+		if ("csv".equals(format)){
+			saveContactsToCsvFile(contacts, file);
+			}
+		else if ("xml".equals(format)){
+			saveContactsToXmlFile(contacts, file);
+			}
+		else{
+			System.out.println("Unknown format" + format);
+			}
+		}
+
+	private static void saveContactsToXmlFile(List<ContactData> contacts, File file) throws IOException {
+		XStream xstream = new XStream();
+		xstream.alias("contact", ContactData.class);
+		String xml = xstream.toXML(contacts);
+		FileWriter writer = new FileWriter(file);
+		writer.write(xml);
+		writer.close();
+	}
+	
+	private static void saveContactsToCsvFile(List<ContactData> contacts, File file) throws IOException {
+		FileWriter writer = new FileWriter(file);
+		for (ContactData contact : contacts){
+			writer.write(contact.getFirstName() + "," + contact.getLastName() + "," + contact.getAddressPrime() + ","
+						+ contact.getHomePhone() + "," + contact.getCellPhone() + "," + contact.getWorkPhone() + "," 
+						+ contact.getBirthDay() + "," + contact.getBirthMonth() + "," + contact.getBirthYear() + "," 
+						+ contact.getEmailPrime() + "," + contact.getEmailSecond() + "," + contact.getAddressSec() + "," 
+						+ contact.getPhoneAdd() + ",!" + "\n");
+		}
+		writer.close();
+	}
+	
+	private static List<ContactData> generateRandomContacts(int amount) {
+		List<ContactData> list = new ArrayList<ContactData>();
+		
+		for (int i = 0; i < amount; i++){
+			ContactData contact = new ContactData()
+			.withFirstName(generateRandomStringName())
+			.withLastName(generateRandomStringName())
+			.withAddressPrime(generateRandomStringAddress())
+			.withAddressSec(generateRandomStringAddress())
+			.withEmailPrime(generateRandomStringEmail())
+			.withEmailSecond(generateRandomStringEmail())
+			.withBirthDay(generateRandomStringBirthDay())
+			.withBirthMonth(generateRandomStringMonth())
+			.withBirthYear(generateRandomStringYear())
+			.withHomePhone(generateRandomStringPhone())
+			.withCellPhone(generateRandomStringPhone())
+			.withWorkPhone(generateRandomStringPhone())
+			.withPhoneAdd(generateRandomStringPhone());
+			
+		    list.add(contact);
+		}
+		return list;
+	}
+	public static String generateRandomStringName(){
+		Random rnd = new Random();
+		if (rnd.nextInt(3) == 0){
+			return "";
+		}
+		else {
+			return "test" + rnd.nextInt();
+		}
+	}
+	
+	public static String generateRandomStringAddress(){
+	Random rnd = new Random();
+	if(rnd.nextInt(3) == 0){
+		return "";
+	}
+	else {
+		return "Address" + rnd.nextInt();
+		}
+	}
+	
+	public static String generateRandomStringEmail(){
+	Random rnd = new Random();
+	if(rnd.nextInt(10) == 0){
+		return "";
+	}
+	else {
+		return "email" + rnd.nextInt(10) + "@gmail.com";
+		}
+	}
+	
+	public static String generateRandomStringBirthDay(){
+		Random rnd = new Random();
+		String birhDay = Integer.toString(rnd.nextInt(30)+1);
+		return birhDay;
+	}
+	
+	public static String generateRandomStringMonth(){
+		List<String> listMonths = new ArrayList<String>();
+			listMonths.add("January");
+			listMonths.add("February");
+			listMonths.add("March");
+			listMonths.add("April");
+			listMonths.add("May");
+			listMonths.add("June");
+			listMonths.add("July");
+			listMonths.add("August");
+			listMonths.add("September");
+			listMonths.add("October");
+			listMonths.add("November");
+			listMonths.add("December");
+		Random rnd = new Random();
+		String month = listMonths.get(rnd.nextInt(11));
+		return month;	
+	}
+	
+	public static String generateRandomStringDigit(){
+		Random rnd = new Random();
+		String digit = Integer.toString(rnd.nextInt(9));
+		return digit;
+	}
+	
+	public static String generateRandomStringPhone(){
+		List<String> listDigit = new ArrayList<String>();
+		for(int i=0; i < 5; i++){
+		String n = generateRandomStringDigit();
+		listDigit.add(n); 
+		
+	}
+		if (Integer.parseInt(listDigit.get(0)) == 0){
+			return "";
+		}
+		else {
+			return listDigit.get(0)+listDigit.get(1)+"-"+listDigit.get(2)+listDigit.get(3)+listDigit.get(4);
+		}
+		
+	}
+	public static String generateRandomStringYear(){
+		String digit = generateRandomStringDigit();
+		return "201"+ digit;
+	
+	}
+	
+}
